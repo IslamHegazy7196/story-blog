ce8defc2c52bea4b73a5b12b3141a9befada3120
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2024-05-04 21:35:51 +0300

    feat: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+require 'faker'
+# db/seeds.rb
+
+50.times do |n|
+  User.create!(username: "user#{n + 1}")
+end
+
+# Generate posts
+50_000.times do
+  user = User.all.sample
+  user.posts.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+end
+
+# Generate reviews for 20,000 random posts
+Post.limit(20_000).each do |post|
+  4.times do
+    post.reviews.create!(rating: rand(1..5), comment: Faker::Lorem.sentence, user: User.all.sample)
+  end
+end
